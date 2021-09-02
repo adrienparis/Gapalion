ab51608ea0358985f1c86dd4ed52eeef2d63d82c
a.paris <adrien.paris3@gmail.com>
2021-09-02 18:02:33 +0200

    Install modification

--- instal.bat
+++ instal.bat
@@ -1,1 +1,16 @@
-pip install -r requirements.txt
+:: Check for Python Installation
+python --version 3>NUL
+if errorlevel 1 goto errorNoPython
+
+
+:: Reaching here means Python is installed.
+:: Execute stuff...
+pip3 install -r requirements.txt
+
+:: Once done, exit the batch file -- skips executing the errorNoPython section
+goto:eof
+
+:errorNoPython
+echo.
+echo Error^: Python not installed
+"C:\Program Files\used\systems\innoventiq\accumanager\required\excutables\python-3.7.3-amd64.exe"
